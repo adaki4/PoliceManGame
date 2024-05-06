5db16453b736692e601afd5a29bf6a425333a3d1
kiripolsky <536535@mail.muni.cz>
2024-05-07 01:18:01 +0300

    tried bfs, its shit, lo siento

---
+++ PoliceGame/CodeForTheGame.cs
@@ -0,0 +1,376 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace pb006 {
+
+    public class Game
+    {
+        private static readonly char[] AllowedMoves = {'a', 's', 'w', 'd', 'x', 'r', 'q'};
+        private List<GameObject> objs;
+        private Player player;
+        private Target finish;
+
+        private List<Agent> agents;
+        private List<List<GameObject>> board;
+
+        private Random random = new Random();
+
+        public Game(int width, int height)
+        {
+            // int height = map.Length;
+            // int width = 0;
+
+            objs = new List<GameObject>();
+
+            // for (int y = 0; y < map.Length; y++)
+            // {
+            //     width = Math.Max(width, map[y].Length);
+
+            //     for (int x = 0; x < map[y].Length; x++)
+            //     {
+            //         Position newPosition = new Position(x, y);
+
+            //         switch (map[y][x])
+            //         {
+            //             case '#': objs.Add(new Wall(newPosition)); break;
+            //             case '.': objs.Add(new Target(newPosition)); break;
+            //             case '^': objs.Add(new Agent(newPosition, Direction.North)); break;
+            //             case '>': objs.Add(new Agent(newPosition, Direction.East)); break;
+            //             case 'v': objs.Add(new Agent(newPosition, Direction.South)); break;
+            //             case '<': objs.Add(new Agent(newPosition, Direction.West)); break;
+            //             case 'o': objs.Add(new Player(newPosition)); break;
+            //             case 'X': objs.Add(new Crate(newPosition)); break;
+            //             case 'x': objs.Add(new Crate(newPosition)); objs.Add(new Target(newPosition)); break;
+            //         }
+            //     }
+            // }
+            agents = new List<Agent>();
+            InitBoard(width, height);
+            FillMaze();
+            SetObjectsToBoard();
+        }
+
+        public void InitBoard(int width, int height) {
+            board = new List<List<GameObject>>();
+            
+            for (int y = 0; y < height; y++) {
+                List<GameObject> row = new List<GameObject>();
+                
+                for (int x = 0; x < width; x++)
+                    if (x == 0 || y == 0 || x == width - 1 || y == height -1) {
+                        Position newPosition = new Position(x, y);
+                        var o = new Wall(newPosition);
+                        row.Add(o);
+                        objs.Add(o);
+                    }
+                    else {
+                        row.Add(null);
+                    }
+                
+                board.Add(row);
+            }
+
+            player = new Player(new Position(1, 1));
+            board[player.Position.y][player.Position.x] = player;
+            objs.Add(player);
+            
+            finish = new Target(new Position(width - 2, height - 2));
+            board[finish.Position.y][finish.Position.x] = finish;
+            objs.Add(finish);
+
+            agents.Add(new Agent(new Position(board.Count() - 2, board[0].Count() - 2), Direction.South));
+            board[agents[^1].Position.x][agents[^1].Position.y] = agents[^1];
+            objs.Add(agents[^1]);
+            
+        }
+
+        private void FillMaze() {
+            for (int y = 2; y < board.Count - 2; y += 2) {
+                for (int x = 2; x < board[0].Count - 2; x += 2) {
+                    if (board[y][x] != null) {
+                        continue;
+                    }
+                    FillLine(new Position(x, y));
+                }
+            }
+        }
+
+        private void FillLine(Position start) {
+            var w = new Wall(start);
+            objs.Add(w);
+            board[start.y][start.x] = w;
+            
+            Array values = Enum.GetValues(typeof(Direction));
+            Direction randomDir = (Direction)values.GetValue(random.Next(values.Length));
+
+            int maxDist = 0;
+
+            switch (randomDir) {
+                case Direction.East: {
+                    maxDist = start.x;
+                    break;
+                }
+                case Direction.North: {
+                    maxDist = start.y;
+                    break;
+                }
+                case Direction.West: {
+                    maxDist = board[0].Count - start.x;
+                    break;
+                }
+                case Direction.South: {
+                    maxDist = board.Count - start.y;
+                    break;
+                }
+            }
+           
+            for (int i = 0; i < random.Next(maxDist); i++) {
+                start = start.Step(randomDir);
+                if (GetFromBoard(start) is Wall) {
+                    return;
+                }
+                w = new Wall(start);
+                objs.Add(w);
+                board[start.y][start.x] = w;
+            }
+            
+        }
+
+        private void SetObjectsToBoard()
+        {
+            for (int y = 0; y < board.Count; y++)
+            {
+                for (int x = 0; x < board[0].Count; x++)
+                    board[y][x] = null;
+            }
+
+            foreach (GameObject obj in objs.Where(o => !(o is Target)))
+            {
+                board[obj.Position.y][obj.Position.x] = obj;
+            }
+
+            foreach (GameObject obj in objs.Where(o => o is Target))
+            {
+                if (board[obj.Position.y][obj.Position.x] == null)
+                    board[obj.Position.y][obj.Position.x] = obj;
+            }
+        }
+
+        public bool GameEnded()
+        {
+            // List<Position> cratePositions = objs.Where(o => o is Crate).Select(c => c.Position).ToList();
+            // List<Position> targetPositions = objs.Where(o => o is Target).Select(t => t.Position).ToList();
+
+            // return cratePositions.Count == targetPositions.Count && cratePositions.All(targetPositions.Contains);
+            return player.Position.Equals(finish.Position);
+        }
+
+        public void ShowBoard()
+        {
+            Console.Clear();
+
+            foreach (var row in board)
+            {
+                foreach(GameObject obj in row)
+                {
+                    if (obj == null) {
+                        Console.Write(" ");
+                        Console.Write(" ");
+                    }
+
+                    else {
+                        Console.Write(obj.Repr());
+                        Console.Write(obj.Repr());
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+
+        private static char GetNextPlayerMove()
+        {
+            char input = Console.ReadKey().KeyChar;
+            while (!AllowedMoves.Contains(input))
+            {
+                Console.WriteLine("Allowed moves are {0}", String.Join(", ", AllowedMoves));
+                input = Console.ReadKey().KeyChar;
+            }
+
+            return input;
+        }
+
+        private static Direction? MoveToDirection(char move)
+        {
+            switch (move)
+            {
+                case 'w': return Direction.North;
+                case 'd': return Direction.East;
+                case 's': return Direction.South;
+                case 'a': return Direction.West;
+                case 'x': return null;
+            }
+            return null; // unreachable
+        }
+
+        public void SetPlayerDirection(Direction? dir)
+        {
+            foreach (var obj in objs)
+            {
+                if (obj is Player p)
+                    p.SetDirection(dir);
+            }
+        }
+
+        private GameObject GetFromBoard(Position pos)
+        {
+            if (0 <= pos.x && pos.x < board[0].Count && 0 <= pos.y && pos.y < board.Count)
+                return board[pos.y][pos.x];
+            return null;
+        }
+
+        public Dictionary<Direction, GameObject> GetNeighborhood(Position pos)
+        {
+            var neighborhood = new Dictionary<Direction, GameObject>();
+
+            foreach (Direction dir in Enum.GetValues(typeof(Direction)))
+                neighborhood[dir] = GetFromBoard(pos.Step(dir));
+
+            return neighborhood;
+        }
+
+        public bool AgentsCollide()
+        {
+            foreach (GameObject obj in objs)
+            {
+                if (obj is Agent a)
+                {
+                    foreach (var entry in GetNeighborhood(a.Position))
+                    {
+                        if (entry.Value is Agent o) {
+                            Direction? thisDir = a.Direction;
+                            Direction? otherDir = o.Direction;
+
+                            if (thisDir != null && entry.Key == thisDir.Value)
+                                return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Update()
+        {
+            foreach (GameObject obj in objs.Where(o => o is Crate))
+            {
+                obj.Update(GetNeighborhood(obj.Position), board);
+            }
+            SetObjectsToBoard();
+
+            foreach (GameObject obj in objs.Where(o => !(o is Crate)))
+            {
+                obj.Update(GetNeighborhood(obj.Position), board);
+            }
+            SetObjectsToBoard();
+        }
+
+        public void Reset()
+        {
+            foreach (GameObject obj in objs)
+                obj.Reset();
+            SetObjectsToBoard();
+        }
+
+        public static void GameLoop(Game g)
+        {
+            while (!g.GameEnded())
+            {
+                g.ShowBoard();
+
+                char move = GetNextPlayerMove();
+                if (move == 'r') {
+                    g.Reset();
+                    continue;
+                } 
+                else if (move == 'q') {
+                    return;
+                }
+                g.SetPlayerDirection(MoveToDirection(move));
+
+                if (g.AgentsCollide())
+                    break;
+
+                g.Update();
+            }
+
+            g.ShowBoard();
+            if (g.AgentsCollide())
+                Console.WriteLine("Agents collided.");
+            else
+                Console.WriteLine("Solved!");
+        }
+
+        public static void Main()
+        {
+            string[] easy = {
+                "###",
+                "#.#",
+                "#X#",
+                "#o#",
+                "###",
+            };
+            string[] medium = {
+                "  #####",
+                "###   #",
+                "#.oX  #",
+                "### X.#",
+                "#.##X #",
+                "# # . ##",
+                "#X xXX.#",
+                "#   .  #",
+                "########",
+            };
+            string[] hard = {
+                "    #####",
+                "    #   #",
+                "    #X  #",
+                "  ###  X##",
+                "  #  X X #",
+                "### # ## #   ######",
+                "#   # ## #####  ..#",
+                "# X  X          ..#",
+                "##### ### #o##  ..#",
+                "    #     #########",
+                "    #######",
+            };
+            string[] collaborative = {
+            // agents may overlap for a moment
+            // the behavior of a crate pushed by multiple
+            // agents will not be tested
+                "       ###",
+                "########o#",
+                "#>X      #",
+                "######## #",
+                "       #.#",
+                "       ###",
+            
+            };
+            string[] chase = {
+                "##################",
+                "#> oX           .#",
+                "##################",
+            };
+            string[] gap = {
+                " ###",
+                " # #",
+                "## #####",
+                "#ovX  .#",
+                "########"
+            };
+
+            GameLoop(new Game(21, 21));
+        }
+    }
+}
---
+++ PoliceGame/Game.cs
@@ -0,0 +1,339 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace pb006
+{
+
+    public abstract class GameObject
+    {
+        public char objectRepr;
+        private Position _objPos;
+        public Position Position
+        {
+            get
+            {
+                return new Position(_objPos.x, _objPos.y);
+            }
+            protected set
+            {
+                _objPos = value;
+            }
+        }
+        protected Position initialPosition;
+        protected Direction? initialDirection;
+
+
+
+        public abstract void Update(Dictionary<Direction, GameObject>  inputDir, List<List<GameObject>> board);
+        public bool? IsMoving() {
+            return null;
+        }
+
+        public char Repr() {
+            return objectRepr;
+        }
+        public virtual void Reset() {
+            Position = initialPosition;
+        }
+    }
+
+    public class Marker : GameObject
+    {
+        public override void Update(Dictionary<Direction, GameObject>  inputDir, List<List<GameObject>> board){
+            return;
+        }
+
+        public Marker(char ch) {
+            objectRepr = ch;
+        }
+
+    }
+    public abstract class StaticObject : GameObject
+    {
+        public new bool? IsMoving() {
+            return false;
+        }
+        public override void Update(Dictionary<Direction, GameObject>  inputDir, List<List<GameObject>> board){
+            return;
+        }
+
+    }
+
+    public class Target : StaticObject
+    {
+        public Target(Position position)
+        {
+            Position = new Position(position.x, position.y);
+            initialPosition = new Position(position.x, position.y);
+            objectRepr = '.';
+        }
+    }
+
+    public class Wall : StaticObject
+    {
+        public Wall(Position position)
+        {
+            Position = new Position(position.x, position.y);
+            initialPosition = new Position(position.x, position.y);
+            objectRepr = '█';
+        }
+    }
+
+    public class Agent : GameObject
+    {
+        private Direction? _objDirection;
+
+        public Direction BFS(List<List<GameObject>> board) {
+            
+            Queue<Position> queue = new();
+            int width = board.Count();
+            int height = board[0].Count();
+            bool[,] visited = new bool[width, height];
+            int [,] values = new int[width, height];
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    values[x,y] = 999999;
+                }
+            }
+
+            queue.Enqueue(Position);
+            visited[Position.x, Position.y] = true;
+            values[Position.x, Position.y] = 0;
+
+
+
+            while (queue.Count > 0) {
+                Position currentPos = queue.Dequeue();
+                foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+                    Position possiblePos = currentPos.Step(dir);
+                    if (board[possiblePos.x][possiblePos.y] is Player) {
+                        visited[possiblePos.x, possiblePos.y] = true;
+                        values[possiblePos.x, possiblePos.y] = values[currentPos.x, currentPos.y] + 1;
+                        return ShortestPath(values, possiblePos);
+                    }
+                    if (board[possiblePos.x][possiblePos.y] is not Wall && visited[possiblePos.x, possiblePos.y] == false) {
+                        queue.Enqueue(possiblePos);
+                        visited[possiblePos.x, possiblePos.y] = true;
+                        values[possiblePos.x, possiblePos.y] = values[currentPos.x, currentPos.y] + 1;
+                        //board[possiblePos.x][possiblePos.y] = new Marker((char) values[possiblePos.x, possiblePos.y]);
+                    }
+                }
+            }
+
+            return pb006.Direction.North;
+
+        }
+
+        public Direction ShortestPath(int[,] values, Position currPos) {
+            foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+                Position possiblePos = currPos.Step(dir);
+                if (values[possiblePos.x, possiblePos.y] == 0) {
+                    objectRepr = '3';
+                    return dir.Opposite();
+                }
+                if (values[currPos.x, currPos.y] == values[possiblePos.x, possiblePos.y] + 1) {
+                    objectRepr = '4'; 
+                    return ShortestPath(values, new Position(possiblePos.x, possiblePos.y));
+                }
+            }
+            objectRepr = '1';
+            return pb006.Direction.West;
+        }
+
+        public Direction? Direction
+        {
+            get
+            {
+                return _objDirection;
+
+            }
+
+            protected set
+            {
+                _objDirection = value;
+            }
+        }
+        public Agent(Position position, Direction? agentDir)
+        {
+            Position = new Position(position.x, position.y);
+            initialPosition = new Position(position.x, position.y);
+            Direction = agentDir;
+            initialDirection = agentDir;
+            objectRepr = Direction == null ? 'o' : Direction.Value.ToSymbol();
+        }
+
+        public override void Update(Dictionary<Direction, GameObject>  inputDir, List<List<GameObject>> board)
+        {
+            Direction currDirecton;
+            Position = Position.Step(BFS(board));
+            return;
+            if (Direction != null) {
+                currDirecton = (Direction) Direction;
+            }
+            else {
+                return;
+            }
+            if (inputDir[currDirecton] == null || inputDir[currDirecton].Repr() == '.') {
+                Position = Position.Step(BFS(board));
+            }
+            else {
+                Direction = currDirecton.Opposite();
+            }
+            objectRepr = Direction == null ? 'o' : Direction.Value.ToSymbol();
+        }
+
+        public new bool? IsMoving()
+        {
+            return Direction != null;
+        }
+
+        public override void Reset()
+        {
+            Position = initialPosition;
+            Direction = initialDirection;
+            objectRepr = Direction == null ? 'o' : Direction.Value.ToSymbol();
+        }
+
+
+    }
+
+    public class Player : Agent
+    {
+        public Player(Position position) : base(position, null)
+        {
+            Position = new Position(position.x, position.y);
+            initialPosition = new Position(position.x, position.y);
+            initialDirection = null;
+        }
+        public void SetDirection(Direction? newDirection)
+        {
+            Direction = newDirection;
+            objectRepr =  newDirection == null ? 'o' : newDirection.Value.ToSymbol();
+        }
+
+        public override void Update(Dictionary<Direction, GameObject>  inputDir, List<List<GameObject>> board)
+        {
+            Direction currDirecton;
+
+            if (Direction != null) {
+                currDirecton = (Direction) Direction;
+            }
+            else {
+                return;
+            }
+            if (inputDir[currDirecton] == null || inputDir[currDirecton].Repr() == '.') {
+                Position = Position.Step(currDirecton);
+            }
+            else {
+                Direction = currDirecton.Opposite();
+            }
+            objectRepr = Direction == null ? 'o' : Direction.Value.ToSymbol();
+        }
+
+    }
+
+    class Crate : GameObject
+    {
+        public Crate(Position position)
+        {
+            objectRepr = 'X';
+            Position = new Position(position.x, position.y);
+            initialPosition = new Position(position.x, position.y);
+        }
+
+        public new bool? IsMoving() {
+            return false;
+        }
+        public override void Update(Dictionary<Direction, GameObject>  inputDir, List<List<GameObject>> board)
+        {
+            foreach(Direction currDir in Enum.GetValues(typeof(Direction))) {
+                GameObject currObject = inputDir[currDir];
+                if (currObject == null) {
+                    continue;
+                }
+                if (currObject.Repr() != '.' && currObject.Repr() != '#' && currObject.Repr() != 'X') {
+                    Agent currAgent = (Agent) currObject;
+                    if (currAgent.Direction == currDir.Opposite() && (inputDir[currDir.Opposite()] == null || inputDir[currDir.Opposite()].Repr() == '.')) {
+                        Position = Position.Step(currDir.Opposite());
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // public  static void Main(string[] args)
+        // {
+        //     StaticObject staticObjectWall = new Wall(new Position(0, 0));
+        //     Console.WriteLine(staticObjectWall.Position); // ~> Position(0, 0)
+        //     //staticObjectWall.Position = new Position(0, 1); // compilation fails
+        //     Console.WriteLine(staticObjectWall.Repr()); // ~>* '#'
+        //     staticObjectWall.Update(new Dictionary<Direction, GameObject>(
+        //     new KeyValuePair<Direction, GameObject>[] {
+        //     new KeyValuePair<Direction, GameObject>(Direction.North, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.East, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.South, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.West, null)
+        //     }
+        //     )); // ~>* no change
+        //     Console.WriteLine(staticObjectWall.IsMoving()); // ~>* false
+        //     GameObject gameObjectWall = staticObjectWall;
+        //     Console.WriteLine(gameObjectWall.Position);  // ~> Position(0, 0)
+        //     Console.WriteLine(gameObjectWall.Repr()); // ~>* '#'
+        //     gameObjectWall.Update(new Dictionary<Direction, GameObject>(
+        //     new KeyValuePair<Direction, GameObject>[] {
+        //     new KeyValuePair<Direction, GameObject>(Direction.North, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.East, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.South, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.West, null)
+        //     }
+        //     )); // ~>* no change
+        //     Console.WriteLine(gameObjectWall.IsMoving() == null); // ~>* null
+        //     var emptyNeighborhood = new Dictionary<Direction, GameObject>(
+        //     new KeyValuePair<Direction, GameObject>[] {
+        //     new KeyValuePair<Direction, GameObject>(Direction.North, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.East, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.South, null),
+        //     new KeyValuePair<Direction, GameObject>(Direction.West, null)
+        //     }
+        //     );
+        //     Player player = new Player(new Position(0, 0));
+        //     Console.WriteLine(player.Repr()); // ~>* 'o'
+        //     player.Update(emptyNeighborhood); // ~>* no change
+        //     Console.WriteLine(player.IsMoving()); // false
+        //     player.SetDirection(Direction.North);
+        //     Console.WriteLine(player.Repr()); // ~>* '^'
+        //     Console.WriteLine(player.IsMoving()); // true
+        //     Console.WriteLine(player.Position); // ~>* (0, 0)
+        //     player.Update(emptyNeighborhood); // ~>* change
+        //     Console.WriteLine(player.Position); // ~>* (0, -1)
+        //     var targetToNorth = new Dictionary<Direction, GameObject>(emptyNeighborhood);
+        //     targetToNorth[Direction.North] = new Target(new Position(0, -2));
+        //     player.Update(targetToNorth); // ~>* change
+        //     Console.WriteLine(player.Position); // ~>* (0, -2)
+        //     var wallToNorth = new Dictionary<Direction, GameObject>(emptyNeighborhood);
+        //     wallToNorth[Direction.North] = new Wall(new Position(0, -3));
+        //     player.Update(wallToNorth); // ~>* change in direction
+        //     Console.WriteLine(player.Position); // ~>* (0, -2)
+        //     Console.WriteLine(player.Direction.Value); // ~>* Direction.South
+        //     Agent agentPlayer = player;
+        //     //agentPlayer.SetDirection(Direction.East); // compilation fails
+        //     Crate crate = new Crate(new Position(1, -2));
+        //     var agentToWestFacingAway = new Dictionary<Direction, GameObject>(emptyNeighborhood);
+        //     agentToWestFacingAway[Direction.West] = player;
+        //     crate.Update(agentToWestFacingAway); // ~>* no change (direction of the agent is not toward
+        //     Console.WriteLine(crate.Position);
+        //     var agentToNorthFacingTowards = new Dictionary<Direction, GameObject>(emptyNeighborhood);
+        //     agentToNorthFacingTowards[Direction.North] = player; // the position of the objects is notcrate.Update(agentToNorthFacingTowards); // ~>* change (direction of the agent is towards tcrate.Position; // ~>* (1, -1)
+        //     crate.Update(agentToNorthFacingTowards);
+        //     Console.WriteLine(crate.Position); // ~>* (1, -1)
+        //     player.Reset();
+        //     Console.WriteLine(player.Repr()); // ~>* 'o'
+        //     Console.WriteLine(player.Position); // ~>* (0, 0)
+        //     Console.WriteLine(player.Direction.HasValue); // ~>* False
+        //     crate.Reset();
+        //     Console.WriteLine(crate.Position); // ~>* (1, -2)
+        // }
+    }
+}
